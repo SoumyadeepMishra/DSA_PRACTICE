47d34b6415b22469c3165567084f2429c022b303
Soumyadeep Mishra <soumyadeepmishra14@gmail.com>
2026-05-13 08:02:47 +0530

    smallest and second smallest

---
+++ PLACEMENT_NQT/Find_largest_and_second_smallest.cpp
@@ -0,0 +1,34 @@
+# include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n ;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+
+    int smallest = INT_MAX;
+    int secondSmallest = INT_MAX;
+
+    for(int i=0;i<n;i++) {
+        if(arr[i] < smallest) {
+            secondSmallest = smallest;
+            smallest = arr[i];
+        }
+        else if(arr[i] < secondSmallest && arr[i] != smallest) {
+            secondSmallest = arr[i];
+        }
+    }
+
+    cout<< "Smallest: " << smallest << endl;
+    if(secondSmallest == INT_MAX) {
+        cout << "No second smallest element found." << endl;
+    } else {
+        cout << "Second Smallest: " << secondSmallest << endl;
+    }
+
+}
